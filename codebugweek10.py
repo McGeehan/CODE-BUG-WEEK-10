4bfb6a4558b53a14072bd6499739aeebb8dfbfae
MkGee <matthewrmcgeehan2@gmail.com>
2024-10-29 11:24:57 -0400

    Add files via upload

---
+++ codebugweek10.py
@@ -0,0 +1,19 @@
+#MAKE THIS CODE PRINT THE FIRST LETTER OF EVERY WORD FROM
+#THE LIST BELOW
+
+#THERE ARE APPROXIMATELY 6 BUGS
+def first_character(text):
+    if text:
+        print(text[3])
+    else:
+        print("Empty string)
+        
+
+#TEST FUNCTION
+if __name__ == "__main__":
+    first_character('python')
+    first_character('yellow')
+    first_character('tomorrow)
+    first_character('heliotrope')
+    first_charact('open')
+    first_char(night))
